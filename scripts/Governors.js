a3006e6dda795c4a33b8e5ec5a4e8c55b6af364d
Laura Beth Maddox <laurabethmaddox@gmail.com>
2022-02-18 11:05:44 -0600

    Governor module updated

--- scripts/Governors.js
+++ scripts/Governors.js
@@ -1,15 +1,32 @@
-// This page will store function for governer selection box and html display
+// Import getter and setter functions for governor data
+import { getGovernors } from "./database.js"
+// This page will store function for governor selection box and html display
 
-// Import getter and setter functions for governer data
+// Declare a new variable that holds return value of governors getter function
+const governors = getGovernors()
 
-// Declare a new variable that holds return value of governers getter function
-
-// Add a change event listener that listens for governer to be changed
+// Add a change event listener that listens for governor to be changed
 // If governor is changed, invoke the setter function
 
 
-// Export and declare a function that displays selection box for governers
+// Export and declare a function that displays selection box for governors
+export const Governors = () => {
+    let html = 
+        `<select id="governor">
+        <option value="0">Choose a governor</option>
+        `
+// Use .map() on governors array for converting objects to <li> elements
+    const listItems = governors.map(
+        (governor) => {
+            return `
+                <option value="${governor.id}">${governor.name}</option>
+            `
+        }
+    )
 
-// Use .map() on governers array for converting objects to <li> elements
+    html += listItems.join("")
+    html += `</select>`
 
 // Return html at the end
+    return html
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
 // Import the exomine.js module
+import { Exomine } from "./Exomine.js"
 
 // Set the variable mainContainer equal to the targeted area class container in index.html
 const mainContainer = document.querySelector("#container")
 
 // Declare a function that will render the HTML in the exomine function 
 const renderAllHTML = () => {
-    mainContainer.innerHTML = // Exomine()
+    mainContainer.innerHTML = Exomine()
 }
 
 // Invoke the function
